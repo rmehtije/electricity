b45bd8c3ed6c1cefa3fa79072bc47fd97399ce99
rmehtije <rmehtije@gmail.com>
2023-09-05 18:28:23 +0300

    date bug fixes

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,11 +3,11 @@ import moment from 'moment';
 const apiUrl = 'https://dashboard.elering.ee/api';
 
 export async function getElectricityPrice({ to, selectedPeriod, from }) {
-    const momentStart = selectedPeriod ? moment().subtract('10', 'hours') : moment(from);
-    const momentEnd = selectedPeriod ? moment().add(selectedPeriod, 'days') : moment(to);
+    const momentStart = selectedPeriod ? moment().subtract('10', 'hours') : moment(from).startOf('day');
+    const momentEnd = selectedPeriod ? moment().add(selectedPeriod, 'days') : moment(to).endOf('day');
 
-    const start = momentStart.toISOString();
-    const end = momentEnd.toISOString();
+    const start = momentStart.toISOString(true);
+    const end = momentEnd.toISOString(true);
 
     const params = new URLSearchParams({
         start,
@@ -20,11 +20,11 @@ export async function getElectricityPrice({ to, selectedPeriod, from }) {
 }
 
 export async function getGasPrice({ to, selectedPeriod, from }) {
-    const momentStart = selectedPeriod ? moment().subtract(selectedPeriod, 'month') : moment(from);
-    const momentEnd = selectedPeriod ? moment() : moment(to);
+    const momentStart = selectedPeriod ? moment().subtract(selectedPeriod, 'month') : moment(from).startOf('day');
+    const momentEnd = selectedPeriod ? moment() : moment(to).endOf('day');
 
-    const start = momentStart.toISOString();
-    const end = momentEnd.toISOString();
+    const start = momentStart.toISOString(true);
+    const end = momentEnd.toISOString(true);
 
     const params = new URLSearchParams({
         start,
